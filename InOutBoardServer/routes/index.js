bbf53e5a1eec44acc9e4a06fd6f86eb23cd93452
Daniel Johansen <dajoh16@student.sdu.dk>
2020-03-04 13:54:59 +0100

    Webserver Create User

--- InOutBoardServer/routes/index.js
+++ InOutBoardServer/routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/api/status', function(req, res, next) {
+
+});
+
+router.get('/api/status/:id', function(req, res, next) {
+
+});
+
+router.post('/api/in/:id', function (req, res, next) {
+
+});
+
+router.post('/api/out/:id', function (req, res, next) {
+
+});
+
 module.exports = router;
+
--- InOutBoardServer/routes/users.js
+++ InOutBoardServer/routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
+var pgp = require('pg-promise')({});
 var router = express.Router();
+var db = pgp('postgres://postgres:dininfo1@167.172.184.103:5432/postgres')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/create', function(req, res, next) {
+    //Name,
+    let name = req.body.name;
+    db.none("INSERT INTO \"user\" (name) values ($1)", name)
+        .then(() => {
+            res.status(200).send(`Created user ${name}`);
+        })
+        .catch(err => {
+            res.status(500).send(err)
+        });
 });
 
 module.exports = router;
