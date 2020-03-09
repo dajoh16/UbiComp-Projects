147c8407ebcde4d3be066754eb1e1556c79b1007
Daniel Johansen <dajoh16@student.sdu.dk>
2020-03-09 10:04:35 +0100

    API Done

--- InOutBoardServer/routes/index.js
+++ InOutBoardServer/routes/index.js
@@ -1,27 +1,59 @@
 var express = require('express');
 var router = express.Router();
-
+var db = require('..\\database');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
 
-router.get('/api/status', function(req, res, next) {
-
+router.get('/api/status', function (req, res, next) {
+    db.many(`SELECT DISTINCT ON (userid) userid, status, dt, tuser.name FROM tevent INNER JOIN tuser AS tuser ON tevent.userid = tuser.id ORDER BY userid, dt DESC`)
+        .then(data => {
+            res.status(200).json(data);
+        }).catch(err => {
+            res.status(404).send();
+    })
 });
 
-router.get('/api/status/:id', function(req, res, next) {
-
+router.get('/api/status/:id', function (req, res, next) {
+    db.one(`SELECT tevent.id, userid, status, dt, tuser.name FROM tevent INNER JOIN tuser AS tuser ON tevent.userid = tuser.id WHERE userid = $1 ORDER BY dt DESC LIMIT 1`, req.params.id)
+        .then(data => {
+            res.status(200).json(data);
+        }).catch((err) => {
+            res.status(404).send();
+    })
 });
 
 router.post('/api/in/:id', function (req, res, next) {
-
+    insertEvent(req, status.IN)
+        .then(data => {
+            res.status(200).json(data);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).send(err);
+        });
 });
 
 router.post('/api/out/:id', function (req, res, next) {
-
+    insertEvent(req, status.OUT)
+        .then(data => {
+            res.status(200).json(data);
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).send(err);
+        });
 });
+
+function insertEvent(req, status) {
+    return db.one(`INSERT INTO tevent (userid, status) VALUES ($1,'${status}') RETURNING *`, req.params.id)
+}
 
 module.exports = router;
 
+const status = {
+    IN: "IN",
+    OUT: "OUT"
+};
--- InOutBoardServer/routes/users.js
+++ InOutBoardServer/routes/users.js
@@ -1,14 +1,12 @@
 var express = require('express');
-var pgp = require('pg-promise')({});
 var router = express.Router();
-var db = pgp('postgres://postgres:dininfo1@167.172.184.103:5432/postgres')
+var db = require('..\\database');
 
 router.post('/create', function(req, res, next) {
-    //Name,
     let name = req.body.name;
-    db.none("INSERT INTO \"user\" (name) values ($1)", name)
-        .then(() => {
-            res.status(200).send(`Created user ${name}`);
+    db.one("INSERT INTO tuser (name) values ($1) RETURNING id,name", name)
+        .then(data => {
+            res.status(200).json(data);
         })
         .catch(err => {
             res.status(500).send(err)
